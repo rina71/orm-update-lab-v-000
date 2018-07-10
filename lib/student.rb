7632b8aa16be4b88999c916c69120376c5d04480
Yurina Shin <yurinas@gmail.com>
2018-07-10 02:09:22 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -53,7 +53,6 @@ class Student
 
   def self.new_from_db(row)
     new_student = self.new
-    binding.pry
     new_student.id = row[0]
     new_student.name = row[1]
     new_student.grade = row[2]

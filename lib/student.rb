c4f2abe2cc090186689e40af2a67bc607f94b912
Yurina Shin <yurinas@gmail.com>
2018-07-10 02:20:12 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -52,10 +52,21 @@ class Student
   end
 
   def self.new_from_db(row)
-  
     Student.new(row[0],row[1],row[2])
 
   end
+
+  def self.find_by_name(name)
+    sql = <<-SQL
+    SELECT name
+    FROM students
+    WHERE name = ?
+    SQL
+    DB[:conn].execute(sql, name)
+
+  end
+
+
 
 
 end
